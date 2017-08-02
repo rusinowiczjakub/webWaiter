290e0f51ca1b6eb63431b82e597e9f86dfe785b4
Piotr Jankiewicz <piotrjankiewicz02@gmail.com>
2017-08-02 14:07:17 +0200

    created entitites: bill, client_order, order

---
+++ webWaiter/src/AppBundle/Entity/Order_element.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Order_element
+ *
+ * @ORM\Table(name="order_element")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\Order_elementRepository")
+ */
+class Order_element
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="quantity", type="integer")
+     */
+    private $quantity;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set quantity
+     *
+     * @param integer $quantity
+     *
+     * @return Order_element
+     */
+    public function setQuantity($quantity)
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+
+    /**
+     * Get quantity
+     *
+     * @return int
+     */
+    public function getQuantity()
+    {
+        return $this->quantity;
+    }
+}
+
