0869db3b9970c04392b7b5eb8ea6fa2d8c1dc12d
Piotr Jankiewicz <piotrjankiewicz02@gmail.com>
2017-08-02 12:51:03 +0200

    added new fosuserbundle to appkernel, created FosuserEntity

---
+++ webWaiter/src/AppBundle/Entity/fosUser.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: piotr
+ * Date: 02.08.17
+ * Time: 12:49
+ */
+
+// src/AppBundle/Entity/User.php
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class fosUser extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
