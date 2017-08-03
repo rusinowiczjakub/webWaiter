ed2f6ad1675b0991f370bf932caee497ecda0d0c
Piotr Jankiewicz <piotrjankiewicz02@gmail.com>
2017-08-03 14:53:09 +0200

    added elements to basket

--- webWaiter/src/AppBundle/Controller/DefaultController.php
+++ webWaiter/src/AppBundle/Controller/DefaultController.php
@@ -15,6 +15,9 @@ class DefaultController extends BaseController
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render("::base.html.twig", ['categories'=>$this->getAllCategories()]);
+        $client_order = $this->getUserOrder();
+        $allElements = $this->getDoctrine()->getManager()->getRepository("AppBundle:Order_element")->findBy(['order'=>$client_order]);
+
+        return $this->render("::base.html.twig", ['categories'=>$this->getAllCategories(), 'order_element'=>$allElements]);
     }
 }
