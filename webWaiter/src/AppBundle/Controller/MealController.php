03685ace98a4dad9b47416cfb0a4be2c8618cf58
Jakub Rusinowicz <rusinowiczjakub@gmail.com>
2017-08-04 14:30:26 +0200

    some fronts, order status

--- webWaiter/src/AppBundle/Controller/MealController.php
+++ webWaiter/src/AppBundle/Controller/MealController.php
@@ -65,73 +65,12 @@ class MealController extends BaseController
      */
     public function showAction(Meal $meal)
     {
-        $deleteForm = $this->createDeleteForm($meal);
 
         return $this->render('meal/show.html.twig', array(
             'meal' => $meal
         ));
     }
 
-    /**
-     * Displays a form to edit an existing meal entity.
-     *
-     * @Route("/{id}/edit", name="meal_edit")
-     * @Method({"GET", "POST"})
-     */
-    public function editAction(Request $request, Meal $meal)
-    {
-        $deleteForm = $this->createDeleteForm($meal);
-        $editForm = $this->createForm('AppBundle\Form\MealType', $meal);
-        $editForm->handleRequest($request);
-
-        if ($editForm->isSubmitted() && $editForm->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
-
-            return $this->redirectToRoute('meal_edit', array('id' => $meal->getId()));
-        }
-
-        return $this->render('meal/edit.html.twig', array(
-            'meal' => $meal,
-            'edit_form' => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
-        ));
-    }
-
-    /**
-     * Deletes a meal entity.
-     *
-     * @Route("/{id}", name="meal_delete")
-     * @Method("DELETE")
-     */
-    public function deleteAction(Request $request, Meal $meal)
-    {
-        $form = $this->createDeleteForm($meal);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->remove($meal);
-            $em->flush();
-        }
-
-        return $this->redirectToRoute('meal_index');
-    }
-
-    /**
-     * Creates a form to delete a meal entity.
-     *
-     * @param Meal $meal The meal entity
-     *
-     * @return \Symfony\Component\Form\Form The form
-     */
-    private function createDeleteForm(Meal $meal)
-    {
-        return $this->createFormBuilder()
-            ->setAction($this->generateUrl('meal_delete', array('id' => $meal->getId())))
-            ->setMethod('DELETE')
-            ->getForm()
-        ;
-    }
 
     /**
      * @Route("/show/{category}", name="show_meals_by_category")
